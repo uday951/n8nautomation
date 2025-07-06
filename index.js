0be12949878d0496ce6438dd89bf47ec98c1b01b
mahathireddy02 <godalamahathi.in@gmail.com>
2025-07-06 15:02:23 +0530

    core

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { chromium } = require('playwright-core'); 
-
+const { chromium } = require('playwright');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
